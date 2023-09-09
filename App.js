76f79236f7a2981bb770ac9e718edab6326abc44
Denesson Barreto <denessonbarretodeoliveira@hotmail.com>
2023-09-09 17:37:39 -0300

    correção na linha 69 da view faltando / na view de fechamento.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {useState} from 'react'
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
@@ -66,7 +66,7 @@ export default function App() {
       <View style={styles.results}>
         <Text style={styles.historyText}>{lastNumber}</Text>
         <Text style={styles.resultText}>{currentNumber}</Text>
-      <View>
+      </View>
 
       {/* Area onde os botões são exibidos*/}
       <View style={styles.buttons}>
